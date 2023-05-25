49327c72f7c23d1427222649d31174db96102dd1
Mads Kampmann Elkjær <madse1998@gmail.com>
2023-05-25 10:35:54 +0200

    Oprettet projekt

---
+++ MarioKart/MarioKart/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * MarioKart.cpp
+ *
+ * Created: 25-05-2023 10:34:41
+ * Author : madse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
